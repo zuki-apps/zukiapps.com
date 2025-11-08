1d7f0eb5968d8428eb38af93a5860fb864ce5f0d
lior pc <zuki.apps@gmail.com>
2025-11-08 15:16:19 +0200

    Disable ESLint during builds

--- next.config.js
+++ next.config.js
@@ -9,6 +9,8 @@ const nextConfig = {
     domains: [],
   },
   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
